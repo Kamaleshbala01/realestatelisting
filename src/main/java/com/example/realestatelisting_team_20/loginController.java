e58dba11103c6eff551f4158e95a3b399b0df8a7
kamaleshbala01 <kamalesravi54@gmail.com>
2023-12-06 21:35:40 +0530

    initial commit

---
+++ src/main/java/com/example/realestatelisting_team_20/loginController.java
@@ -0,0 +1,16 @@
+package com.example.realestatelisting_team_20;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class loginController implements Initializable {
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
---
+++ src/main/java/com/example/realestatelisting_team_20/signupController.java
@@ -0,0 +1,14 @@
+package com.example.realestatelisting_team_20;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class signupController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
