7048be9dd8f63e91656ac000e2989ebd3654abf3
kamaleshbala01 <kamalesravi54@gmail.com>
2023-12-07 10:46:15 +0530

    initial commit

---
+++ src/main/java/com/example/realestatelisting_team_20/DataBaseConnection.java
@@ -0,0 +1,28 @@
+package com.example.realestatelisting_team_20;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DataBaseConnection {
+    static String user = "root";
+    static String password = "kamalesh@2005";
+    static String url = "jdbc:mysql://localhost:3306/realestatelisting";
+    static String driver = "com.mysql.cj.jdbc.Driver";
+    public static Connection getConnection(){
+        Connection connection = null;
+        try {
+            Class.forName(driver);
+        }
+        catch(ClassNotFoundException e){
+               throw new RuntimeException(e);
+        }
+        try {
+            connection = DriverManager.getConnection(url, user, password);
+        }
+        catch(SQLException e){
+            throw new RuntimeException(e);
+        }
+        return connection;
+    }
+}
---
+++ src/main/java/com/example/realestatelisting_team_20/homeController.java
@@ -0,0 +1,14 @@
+package com.example.realestatelisting_team_20;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class homeController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
