56d9f3b78baf6a490cc9a1a55263cbdcd07c4c6b
Kamalesh Bala <139665559+Kamaleshbala01@users.noreply.github.com>
2025-02-23 10:17:41 +0530

    Update DataBaseConnection.java

--- src/main/java/com/example/realestatelisting_team_20/DataBaseConnection.java
+++ src/main/java/com/example/realestatelisting_team_20/DataBaseConnection.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class DataBaseConnection {
     static String user = "root";
-    static String password = "kamalesh@2005";
+    static String password = "";
     static String url = "jdbc:mysql://localhost:3306/realestatelisting";
     static String driver = "com.mysql.cj.jdbc.Driver";
     public static Connection getConnection(){
